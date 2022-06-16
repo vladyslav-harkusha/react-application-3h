655839970b8e87c0adc994c640acd4063452e370
Vlad Harkusha <sivari888@gmail.com>
2022-06-16 11:38:50 +0300

    add sorting functional

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { useState } from 'react';
 // import { ClassCounter } from "./components/ClassCounter";
 import { PostList } from './components/PostLIst';
 import { PostForm } from './components/PostForm';
+import { MySelect } from "./components/UI/select/MySelect";
 import './styles/App.css';
 
 export const App = () => {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'Pyton', body: 'Description' },
-    { id: 3, title: 'PHP', body: 'Description' },
-    { id: 4, title: 'Cotlin', body: 'Description' },
+    { id: 1, title: 'JavaScript', body: 'Восхитительный' },
+    { id: 2, title: 'Pyton', body: 'Отличный' },
+    { id: 3, title: 'PHP', body: 'Замечательый' },
+    { id: 4, title: 'Cotlin', body: 'Суперский' },
   ]);
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -22,14 +24,31 @@ export const App = () => {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+
+    setPosts([...posts].sort((post1, post2) => post1[sort].localeCompare(post2[sort])));
+  }
+
   return (
     <div className="App">
-      {/* <Counter /> */}
-      {/* <ClassCounter /> */}
-
       <PostForm create={createPost} />
 
-      {posts.length !== 0
+      <hr style={{margin: "15px 0"}} />
+
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
+      </div>
+
+      {posts.length
         ? <PostList remove={removePost} posts={posts} title="Список языков программирования" />
         : <h2 style={{color: "orange", textAlign: "center"}}>
             Посты не найдены!
