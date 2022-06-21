b08954fb808a02bb729e31997dc39eaf75c6d4f6
Vlad Harkusha <sivari888@gmail.com>
2022-06-21 15:20:23 +0300

    relocate pagination styles

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -1,15 +1,16 @@
 import { getPagesArray } from "../../../utils/pages";
+import cl from "./Pagination.module.css";
 
 export const Pagination = ({ totalPages, page, changePage }) => {
   let pagesArray = getPagesArray(totalPages);
 
   return (
-    <div className="pages__wrapper">
+    <div className={cl.pages__wrapper}>
       {pagesArray.map(p =>
         <span
           onClick={() => changePage(p)}
           key={p}
-          className={page === p ? "pages pages--current" : "pages"}
+          className={page === p ? `${cl.pages} ${cl.pages__current}` : cl.pages}
         >
           {p}
         </span>
