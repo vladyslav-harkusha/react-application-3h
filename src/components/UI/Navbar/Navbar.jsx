e620fb71981234e993b36e7b1680426b6d855027
Vlad Harkusha <sivari888@gmail.com>
2022-07-29 19:08:53 +0300

    fix links

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -23,15 +23,15 @@ export const Navbar = () => {
 
       <ul className="navbar__links">
         <li className="navbar__link">
-          <Link to="/about">About</Link>
+          <Link to="/react-application-3h/about">About</Link>
         </li>
 
         <li className="navbar__link">
-          <Link to="/posts">Posts (with pagination)</Link>
+          <Link to="/react-application-3h/posts">Posts (with pagination)</Link>
         </li>
 
         <li className="navbar__link">
-          <Link to="/posts_infinity_scroll">Posts (infinity scroll)</Link>
+          <Link to="/react-application-3h/posts_infinity_scroll">Posts (infinity scroll)</Link>
         </li>
       </ul>
     </div>
--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,16 @@ import { Login } from "../pages/Login";
 import { PostsInfinityScroll } from "../pages/PostsInfinityScroll";
 
 export const privateRoutes = [
-  { path: "/about", element: <About /> },
-  { path: "/posts", element: <Posts /> },
-  { path: "/posts_infinity_scroll", element: <PostsInfinityScroll /> },
-  { path: "/posts/:id", element: <PostIdPage /> },
-  { path: "/errorr", element: <Errorr /> },
-  { path: "/login", element: <Navigate to="/posts" replace /> },
-  { path: "/", element: <Navigate to="/posts" replace /> },
-  { path: "/*", element: <Navigate to = "/errorr" replace /> },
+  { path: "/react-application-3h/about", element: <About /> },
+  { path: "/react-application-3h/posts", element: <Posts /> },
+  { path: "/react-application-3h/posts_infinity_scroll", element: <PostsInfinityScroll /> },
+  { path: "/react-application-3h/posts/:id", element: <PostIdPage /> },
+  { path: "/react-application-3h/errorr", element: <Errorr /> },
+  { path: "/react-application-3h/login", element: <Navigate to="/react-application-3h/posts" replace /> },
+  { path: "/react-application-3h/", element: <Navigate to="/react-application-3h/posts" replace /> },
 ];
 
 export const publicRoutes = [
-  { path: "/login", element: <Login /> },
-  { path: "/*", element: <Navigate to="/login" replace /> },
+  { path: "/react-application-3h/login", element: <Login /> },
+  { path: "/*", element: <Navigate to="/react-application-3h/login" replace /> },
 ];
